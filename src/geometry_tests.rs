8e4c5e8b402bbe8d54ae86dd7d6e437e34233a87
Alexander Sergeev <etc9053@gmail.com>
2021-11-08 20:16:58 +0300

    Fix rects

--- src/geometry_tests.rs
+++ src/geometry_tests.rs
@@ -5,7 +5,7 @@ mod geometry_tests {
 
     #[test]
     fn test_iter() {
-        let cords = Coordinates::new(5, 15, 10, 15);
+        let cords = Coordinates::new((5, 15), (10, 15));
         assert!(cords.x_iter().count() == 11);
         assert!(cords.y_iter().count() == 6);
         assert!(cords.into_iter().count() == 66);
--- src/widgets.rs
+++ src/widgets.rs
@@ -8,10 +8,10 @@ impl Framebuffer {
         }
 
         self.draw_rect(cords, color);
-        let position = cords.start.0 + ((cords.end.0 - cords.start.0) as f32 * (progress as f32 / 100.0)) as i32;
+        let position = cords.start.0 + (cords.dx() as f32 * (progress as f32 / 100.0)) as i32;
         for x in cords.x_iter().skip(1) {
             let color = if x <= position { Color::Black } else { Color::White };
-            self.draw_vertical_line(x,cords.start.1 + 2, cords.end.1 - 3, &color);
+            self.draw_vertical_line(x,cords.start.1 + 1, cords.end.1 - 1, &color);
         }
 
         true
@@ -22,8 +22,8 @@ impl Framebuffer {
         if orient == Orientation::Vertical {
             self.draw_filled_rect(cords, &Color::White);
 
-            let mid_x = (cords.end.0 - cords.start.0) / 2;
-            self.draw_vertical_line( mid_x, cords.start.1, cords.end.1, color);
+            let mid_x = cords.dx() / 2;
+            self.draw_vertical_line(mid_x, cords.start.1, cords.end.1, color);
             let position = cords.start.1 + ((cords.end.1 - width - cords.start.1) as f32 * (position as f32 / 100.0)) as i32;
 
             self.draw_rect(&Coordinates::new(
